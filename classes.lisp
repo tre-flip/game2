b59fd6f46dd2274df678209ce528acd073f9bfa6
tre-flip <none>
2020-10-12 21:25:39 +0300

    working on collision detection

--- classes.lisp
+++ classes.lisp
@@ -2,9 +2,15 @@
 
 (in-package #:game2)
 
+(defgeneric collide-p (a b)
+  (:documentation "Checks if A and B collide")) 
+
 (defclass collidable ()
   ((collision-radius :initform 5
 		     :initarg :collision-radius
 		     :accessor collision-radius
 		     :documentation "Used for collision detection."))
   (:documentation "Collidable objects can collide with each other. Assumes, this object has X and Y coordinates!"))
+
+(defmethod collide-p ((a collidable) (b collidable))
+  )
